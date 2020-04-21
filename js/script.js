23b84106e9de91fdc3c88dc6a1266cff1967f128
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-04-20 22:42:15 -0300

    mobile pagination

--- js/script.js
+++ js/script.js
@@ -5,49 +5,75 @@ let pgW = 400,
 
 let pageIndex = 4;
 let pages = document.querySelectorAll(".page");
+let mobilePaginationScheme = false;
+let pageOffset = 2;
+let flipDelay = 250;
+
 function detectMobile() {
   let w = Math.max(
     document.documentElement.clientWidth,
     window.innerWidth || 0
   );
   mobile = w < smBrkpnt;
-  if (mobile) {
-    document.body.style.backgroundColor = "cyan";
-  } else {
-    document.body.style.backgroundColor = "#ffd21f";
+
+  // detects a change in size which should redo the pagination
+  if (mobile && !mobilePaginationScheme) {
+    mobilePaginationScheme = true;
+    pageOffset = 1;
+    flipDelay = 0;
+  } else if (!mobile && mobilePaginationScheme) {
+    mobilePaginationScheme = false;
+
+    // if current page is even i'll set index to its
+    // even pair
+    if (pageIndex % 2 != 0) {
+      pageIndex -= 1;
+    }
+    pageOffset = 2;
+    flipDelay = 250;
   }
+}
+
 function advancesPages() {
   // are there more pages to flip to?
-  if (pageIndex + 2 < pages.length) {
+  if (pageIndex + pageOffset < pages.length) {
     pages[pageIndex].classList.remove("current");
     pages[pageIndex].classList.add("past");
     pages[pageIndex + 1].classList.add("past");
 
-    pages[pageIndex + 1].classList.add("zoom-top");
-
-    setTimeout(() => {
-      pages[pageIndex + 1].classList.remove("zoom-top");
-      pageIndex += 2;
+    if (!mobilePaginationScheme) {
+      pages[pageIndex + 1].classList.add("zoom-top");
+      setTimeout(() => {
+        pages[pageIndex + 1].classList.remove("zoom-top");
+        pageIndex += pageOffset;
+        pages[pageIndex].classList.add("current");
+      }, flipDelay);
+    } else {
+      pageIndex += pageOffset;
       pages[pageIndex].classList.add("current");
-    }, 250);
+    }
   }
 }
 
 function returnsPages() {
   // have we reached the start of the book?
-  if (pageIndex - 2 >= 0) {
+  if (pageIndex - pageOffset >= 0) {
     pages[pageIndex].classList.remove("current");
 
-    pages[pageIndex + 1].classList.add("zoom-top");
+    if (!mobilePaginationScheme) {
+      pages[pageIndex + 1].classList.add("zoom-top");
+    }
 
-    pageIndex -= 2;
+    pageIndex -= pageOffset;
     pages[pageIndex].classList.remove("past");
     pages[pageIndex].classList.add("current");
 
     setTimeout(() => {
-      pages[pageIndex + 2 + 1].classList.remove("zoom-top");
+      if (!mobilePaginationScheme) {
+        pages[pageIndex + pageOffset + 1].classList.remove("zoom-top");
+      }
       pages[pageIndex + 1].classList.remove("past");
-    }, 250);
+    }, flipDelay);
   }
 }
 
