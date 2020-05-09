c921a2f0b22e709e043327bbf830496ad2dfe543
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-05-08 23:38:53 -0300

    creates diary links after mock data

--- js/diaries-index.js
+++ js/diaries-index.js
@@ -9,7 +9,59 @@ function setStyles() {
 
 function setupDiariesLinks() {
   mockData.forEach((diary) => {
-    console.log(diary);
+    let bgCode = coverColorCode(
+      diary.initials,
+      diary.location,
+      diary.age,
+      diary.profession,
+      diary.gender
+    );
+    let inkCode = inkColorCode(
+      diary.initials,
+      diary.location,
+      diary.age,
+      diary.profession,
+      diary.gender
+    );
+
+    let initials = "",
+      age = "";
+    for (let i = 0; i < diary.initials.length; i++) {
+      initials += diary.initials[i] + ".";
+    }
+
+    if (diary.age != "") {
+      age = ", " + diary.age + " anos";
+    }
+
+    let diaryLink = document.createElement("div");
+    diaryLink.classList.add("diary-link", `cover_${bgCode}`, `ink_${inkCode}`);
+
+    // the heading
+    let header = document.createElement("header");
+    let h1 = document.createElement("h1");
+    h1.innerHTML = initials + age;
+    header.appendChild(h1);
+    if (diary.location != "") {
+      let h2 = document.createElement("h2");
+      h2.innerHTML = diary.location;
+      header.appendChild(h2);
+    }
+    diaryLink.appendChild(header);
+
+    // the decoration
+    let deco = document.createElement("div");
+    deco.classList.add("diary-decoration");
+    diaryLink.append(deco);
+
+    // the link & eventually etc
+    let anchor = document.createElement("a");
+    anchor.title = `Acesse o diário de ${initials}`;
+    anchor.href = diary.link;
+    anchor.appendChild(document.createTextNode("Ler este diário"));
+    diaryLink.appendChild(anchor);
+
+    diariesList.appendChild(diaryLink);
   });
 }
 
